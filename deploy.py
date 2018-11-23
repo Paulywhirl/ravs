82acc27250bf9458224820df935b118cd64669c5
paulywhirl <plhenderson12@hotmail.com>
2018-11-23 12:00:55 -0500

    made some changes with sqlAlchemy database creation

---
+++ deploy.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_heroku import Heroku
+
+app = Flask(__name__, template_folder='../templates/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/ravs-database'
+# heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+# Create our database model
+# class User(db.Model):
+#     __tablename__ = "users"
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True)
+#     name = db.Column(db.String(120), unique=False)
+#
+#     def __init__(self, email):
+#         self.email = email
+#
+#     def __repr__(self):
+#         return '<E-mail %r>' % self.email
+
+class Members(db.Model):
+    __tablename__ = "members"
+    member_id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(25), nullable=False)
+    lastname = db.Column(db.String(35), nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    phone = db.Column(db.String(12), unique=True)
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"Members('{self.firstname}', '{self.lastname}', '{self.email}')"
+
+class Progress_Graph(db.Model):
+    __tablename__ = "progress_graph"
+    id = db.Column(db.Integer, primary_key=True)
+
+
+
+# Set "homepage" to index.html
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+# Save e-mail to database and send to success page
+# @app.route('/prereg', methods=['POST'])
+# def prereg():
+#     email = None
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         # Check that email does not already exist (not a great query, but works)
+#         if not db.session.query(User).filter(User.email == email).count():
+#             reg = User(email)
+#             db.session.add(reg)
+#             db.session.commit()
+#             return render_template('success.html')
+#     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
