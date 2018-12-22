31874abf6647236acf34a767f74fbafb6c05451b
Emily Popovic <epopovic@uwo.ca>
2018-12-22 15:18:43 -0500

    editied the UI design

---
+++ src/components/Sidebar.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import './Sidebar.scss';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div class="sidebar">
+        <nav class="sidebar-nav">
+          <ul class="nav">
+            <li class="nav-title">Overview</li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="nav-icon cui-speedometer"></i> Dashboard
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="nav-icon cui-speedometer"></i> Calender
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="nav-icon cui-speedometer"></i> Annoucements
+              </a>
+            </li>
+            <li class="nav-title">User</li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="nav-icon cui-speedometer"></i> Profile
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="nav-icon cui-speedometer"></i> Progress
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="nav-icon cui-speedometer"></i> Settings
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="nav-icon cui-speedometer"></i> Logout
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+    )
+  }
+}
+
+export default Sidebar;
