675cbe98df3ae4f513a9f2287c18fe9f61354366
paulywhirl <plhenderson12@hotmail.com>
2019-03-19 14:36:30 -0400

    corrected mixup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Redirect} from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 
 import Header from './components/Header';
 import Login from './Login';
